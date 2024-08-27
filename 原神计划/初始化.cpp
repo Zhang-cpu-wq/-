59b50576429f3011de9952c94c37e6bf9aa93095
Zhang-cpu-wq <153421558+Zhang-cpu-wq@users.noreply.github.com>
2024-08-27 14:39:11 +0800

    Add files via upload

---
+++ 原神计划/初始化.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<windows.h>
+using namespace std;
+
+int main(){
+	cout<<"yuanshen";
+	cout<<"\r";
+	Sleep(5000);
+	cout<<"  qidong\n";
+	system("pause");
+	return 0;
+}
+
